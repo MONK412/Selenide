06fdadb168096acfb4092d800c5287e88ed263f9
Rustam Mustafin <rustammuus@mail.ru>
2022-07-31 21:34:01 +0300

    Fix Date, Spin And Name

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -5,27 +5,33 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
-    @Test
-    void test1() {
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
+    @Test
+    void shouldShowSuccessfulCompletionOfTheBooking() {
+
+        String planningDate = generateDate(4);
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Калининград");
-//        $("[class='menu-item__control']").click();
         $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue("05.09.2022");
+        $("[data-test-id='date'] input").sendKeys(planningDate);
         $("[data-test-id='name'] input").setValue("Мунир-Ислям Рустам");
         $("[data-test-id='phone'] input").setValue("+75864127846");
         $("[data-test-id='agreement']").click();
         $x("//span[@class='button__text']").click();
-        $("[class='spin spin_size_m spin_visible spin_theme_alfa-on-color']").
+        $(".spin").
                 shouldBe(visible);
         $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
     }
